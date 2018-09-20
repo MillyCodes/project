bca9a139dfeb585c0168ed9e3e33d40b01fcab70
Milly R <millz118@gmail.com>
2018-09-20 16:01:01 -0400

    got info from all three apis to render

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,6 @@
 class ArtistsController < ApplicationController
   require 'rspotify'
   RSpotify.authenticate(ENV["SPOTIFY_KEY"], ENV["SPOTIFY_SECRET"])
-  require 'lastfm'
-
   
   # token = lastfm.auth.get_token
   # lastfm.session = lastfm.auth.get_session(token: token)['key']
@@ -11,13 +9,15 @@ class ArtistsController < ApplicationController
   def index
     if !params[:artist_name].empty?
         lastfm = Lastfm.new(ENV["LASTFM_KEY"], ENV["LASTFM_SECRET"])
+        @videos = Yt::Collections::Videos.new
         artistspotify = RSpotify::Artist.search(params[:artist_name])
         artistlastfm = lastfm.artist.get_info(artist: params[:artist_name])
         
         @artistbio = artistlastfm["bio"]["summary"]
+        puts artistspotify
         @artist = artistspotify.first
         @tracks = @artist.top_tracks(:US)
-    
+        # @ytvideos = @videos.where(q: params[:artist_name], safe_search: 'none').first.title
     else
         redirect_to root_path
     end  

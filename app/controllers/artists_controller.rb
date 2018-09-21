ab48b5c459e68d7c65323adcfb9182fdb79abfa3
Milly R <millz118@gmail.com>
2018-09-21 00:55:42 -0400

    fixed youtubekey failures

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,9 @@
 class ArtistsController < ApplicationController
   require 'rspotify'
   RSpotify.authenticate(ENV["SPOTIFY_KEY"], ENV["SPOTIFY_SECRET"])
-  
+  Yt.configure do |config|
+    config.api_key = ENV["YOUTUBE_KEY"]
+  end
   # token = lastfm.auth.get_token
   # lastfm.session = lastfm.auth.get_session(token: token)['key']
 
@@ -11,13 +13,14 @@ class ArtistsController < ApplicationController
         lastfm = Lastfm.new(ENV["LASTFM_KEY"], ENV["LASTFM_SECRET"])
         @videos = Yt::Collections::Videos.new
         artistspotify = RSpotify::Artist.search(params[:artist_name])
-        artistlastfm = lastfm.artist.get_info(artist: params[:artist_name])
+        artistlastfm = lastfm.artist.get_info(artist: params[:artist_name], autocorrect: 1)
         
         @artistbio = artistlastfm["bio"]["summary"]
         puts artistspotify
         @artist = artistspotify.first
         @tracks = @artist.top_tracks(:US)
-        # @ytvideos = @videos.where(q: params[:artist_name], safe_search: 'none').first.title
+        # <% @videoId = @videos.where(q: "music video for #{@artist.name} #{track.name}", order: 'relevance').first.id %>
+        # @videoId = @videos.where(q: params[:artist_name], safe_search: 'none').first.id
     else
         redirect_to root_path
     end  
